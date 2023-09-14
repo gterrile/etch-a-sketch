ca3c13ba907bf1b3551bf928ec6e7958380eff50
German Terrile <gterrile@gmail.com>
2023-09-14 09:44:52 -0700

    color and black paint functions working

--- script.js
+++ script.js
@@ -1,50 +1,37 @@
-const gridSize = 700;
-var totalElements = 256;
-var divSize = (gridSize / 16);
+// Elements consts
 const containerGrid = document.getElementById('containerGrid');
-var slider = document.getElementById('slider');
-
 const style = document.getElementById('style');
+const slider = document.getElementById('slider');
+
+// Grid variables
+const gridSize = 700;
+let totalElements = slider.value * slider.value;
+let divSize = (gridSize / slider.value);
+// var divs = document.querySelectorAll('.divElement');
+
 
 // Get mouse click status
 var mouseStatus = 0;
-window.onmousedown = function () {
+window.onmousedown = function () { 
   mouseStatus = 1;
+  console.log(mouseStatus);
 }
-window.onmouseup = function () {
-  mouseStatus = 0;
+window.onmouseup = function () { 
+  mouseStatus = 0; 
+  console.log(mouseStatus);
 }
 
-// Create default new child elements inside the grid
-for (i = 1; i <= totalElements; i++) {
-  const divElement = document.createElement('div');
-  divElement.classList.add('divElement');
-  containerGrid.appendChild(divElement);
+// Colored grid
+function createColoredGrid () {
   
-  // get random values for rgb
-  const r = Math.random() * 255;
-  const g = Math.random() * 255;
-  const b = Math.random() * 255;
-
-  divElement.setAttribute('style', `height:${divSize}px; min-width:${divSize}px; 
-  background-color: rgb(${r},${g},${b}); opacity: 0.1`);
-}
-
-var divs = document.querySelectorAll('.divElement');
-
-// Slider changes
-slider.oninput = function () {  
-  
-  // Update grid values
-  totalElements = this.value * this.value;
-  divSize = (gridSize / this.value);
+  let totalElements = slider.value * slider.value;
+  let divSize = gridSize / slider.value;
 
   // Remove all child nodes of the grid
   while (containerGrid.firstChild) {
     containerGrid.removeChild(containerGrid.firstChild);
   }
-
-  // Create new child elements inside the grid
+  
   for (i = 1; i <= totalElements; i++) {
     const divElement = document.createElement('div');
     divElement.classList.add('divElement');
@@ -55,45 +42,110 @@ slider.oninput = function () {
     const g = Math.random() * 255;
     const b = Math.random() * 255;
 
-    // Style the elements with random colors
     divElement.setAttribute('style', `height:${divSize}px; min-width:${divSize}px; 
-    background-color: rgb(${r},${g},${b}); opacity: 0.1`); 
+    background-color: rgb(${r},${g},${b}); opacity: 0.1`);
+  }
+  
+  let divs = document.querySelectorAll('.divElement');
+  paintColor(divs)
+  return divs;
+}
+
+// Grayscale grid
+function createGrayscaleGrid () {
+  
+  let totalElements = slider.value * slider.value;
+  let divSize = gridSize / slider.value;
+  
+  // Remove all child nodes of the grid
+  while (containerGrid.firstChild) {
+    containerGrid.removeChild(containerGrid.firstChild);
   }
 
-  // update nodeList of divElement class elements.
-  divs = document.querySelectorAll('.divElement');
+  for (i = 1; i <= totalElements; i++) {
+    const divElement = document.createElement('div');
+    divElement.classList.add('divElement');
+    containerGrid.appendChild(divElement);
+
+    // get random values for rgb
+    const x = Math.random() * 255;
+
+    divElement.setAttribute('style', `height:${divSize}px; min-width:${divSize}px; 
+    background-color: rgb(${x},${x},${x}); opacity: 0.1`);
+  }
+  let divs = document.querySelectorAll('.divElement');
+  paintBlack(divs);
+  return divs;
+}
+
+let lala = createColoredGrid();
+paintColor(lala, slider.value);
+
+function paintColor(nodes) {
   
-  // Paint elements in the grid
-  divs.forEach(item => {
+  let totalElements = slider.value * slider.value;
+  let divSize = gridSize / slider.value;
+  
+  nodes.forEach(item => {
   item.addEventListener ('mouseover', (e) => {
-      if (mouseStatus) {
-        const r = Math.random() * 255;
-        const g = Math.random() * 255;
-        const b = Math.random() * 255;
-    
-        item.setAttribute('style', `height:${divSize}px; min-width:${divSize}px; 
-        background-color: rgb(${r},${g},${b})`);
-        item.classList.add('colored');
-      }
+        if (mouseStatus) {
+          const r = Math.random() * 255;
+          const g = Math.random() * 255;
+          const b = Math.random() * 255;
+          item.setAttribute('style', `height:${divSize}px; min-width:${divSize}px; 
+          background-color: rgb(${r},${g},${b})`);
+          item.classList.add('colored');
+        }
     })
   })
 }
 
-// Paint elements in the grid
-divs.forEach(item => {
-  item.addEventListener ('mouseover', (e) => {
-    if (mouseStatus) {
-      const r = Math.random() * 255;
-      const g = Math.random() * 255;
-      const b = Math.random() * 255;
+function paintBlack(nodes) {
   
-      item.setAttribute('style', `height:${divSize}px; min-width:${divSize}px; 
-      background-color: rgb(${r},${g},${b})`);
-      item.classList.add('colored');
-    }  
+  let totalElements = slider.value * slider.value;
+  let divSize = gridSize / slider.value;
+  
+  nodes.forEach(item => {
+  item.addEventListener ('mouseover', (e) => {
+        if (mouseStatus) {
+          item.setAttribute('style', `height:${divSize}px; min-width:${divSize}px; 
+          background-color: rgb(0,0,0)`);
+          item.classList.add('colored');
+        }
+    })
   })
-})
+}
 
-var switchMode = document.getElementById('switchMode');
 
-console.log(switchMode);
+// Slider changes
+slider.oninput = function () {  
+  if (style.value == 1) {
+    createColoredGrid();
+  }
+
+  if (style.value == 2) {
+    createGrayscaleGrid();
+  }
+}
+
+// Style changes
+style.onchange = function () {
+  if (this.value == 1) {
+    createColoredGrid();
+  }
+
+  if (this.value == 2) {
+    createGrayscaleGrid();
+  }
+}
+
+// if (mouseStatus) {
+//   paintColor();
+// }
+
+
+// var divs = document.querySelectorAll('.divElement');
+
+
+
+
